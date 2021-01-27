d66d2462522bda1f7f91bcc662d1b3846ef17e1c
chernov <kapot65@gmail.com>
2021-01-27 14:08:17 +0300

    Minor fixes - Add confirmation dialog (resolve #12 resolve #8) (#8 cant be done fully because of difference between the DeviceWidget and PlotWidget configuration logic)

--- src/utils/header/CloseButton.tsx
+++ src/utils/header/CloseButton.tsx
@@ -1,12 +1,42 @@
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import React from "react";
+import React, {useState} from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
 
-export const CloseButton = (props: {onClose: () => any}) => (<IconButton
-        style={{
-        float: "right",
-            padding: "12px"
-    }}
-    onClick={props.onClose}>
-    <CloseIcon/>
-</IconButton>)
+export const CloseButton = (props: {onClose: () => any}) => {
+
+    const [open, setOpen] = useState(false)
+    return <>
+        <IconButton
+            style={{
+                float: "right",
+                padding: "12px"
+            }}
+
+            onClick={() => setOpen(true)}>
+            <CloseIcon/>
+        </IconButton>
+        <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+            <DialogActions>
+                <Button onClick={() => setOpen(false)} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={() => {
+                    setOpen(false);
+                    props.onClose()
+                }} color="primary" autoFocus>
+                    Confirm
+                </Button>
+            </DialogActions>
+        </Dialog>
+    </>
+}
